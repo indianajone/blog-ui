3871e16955c1b26add21fb308d43580a744fbc6d
indianajone <mmer555@hotmail.com>
2017-10-23 02:21:38 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="container-fluid">
+    <div class="hero is-primary">
+        <div class="container">
+            <div class="hero-body">   
+                <h1 class="title">
+                  <a href="/">My Blog</a>
+                </h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="scss">
+@import '~bulma';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="posts"></div>
+</template>
+
+<script>
+export default {
+  name: 'Posts',
+};
+</script>
+
+<style scoped lang="scss">
+</style>
