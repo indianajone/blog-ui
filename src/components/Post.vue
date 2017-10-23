dfa8112fafe28bc52a5bd28af68690c958616048
indianajone <mmer555@hotmail.com>
2017-10-23 11:18:15 +0700

    add plain text view

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -3,22 +3,35 @@
         <router-link class="button ml-auto is-primary is-outlined" :to="'/post/edit/' + post._id">
             Edit
         </router-link>
+        <component :is="view" :post="post"></component>
     </div>
 </template>
 
 <script>
+import TextView from '@/components/views/TextView';
+
 export default {
     name: 'Post',
+    components: { TextView, ImageView },
     data() {
         return {
             post: {},
+            view: null
         };
+    },
+    mounted() {
+        this.fetch().then(() => {
+            this.loadView();
+        });
     },
     methods: {
         async fetch() {
             const { id } = this.$route.params;
             const { data } = await this.$http.get(`/posts/${id}`);
             this.post = data.data;
+        },
+        loadView() {
+            this.view = `${this.titleCase(this.post.type)}View`;
         },
         titleCase(str) {
             return str.charAt(0).toUpperCase() + str.slice(1);
