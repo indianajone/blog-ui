666237be3c35ea959aab7da9a9c68ddf8bb1f774
indianajone <mmer555@hotmail.com>
2017-10-23 11:17:33 +0700

    add single post page

---
+++ src/components/Post.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="section is-flex is-verticle is-block-center">
+        <router-link class="button ml-auto is-primary is-outlined" :to="'/post/edit/' + post._id">
+            Edit
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Post',
+    data() {
+        return {
+            post: {},
+        };
+    },
+    methods: {
+        async fetch() {
+            const { id } = this.$route.params;
+            const { data } = await this.$http.get(`/posts/${id}`);
+            this.post = data.data;
+        },
+        titleCase(str) {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+    .post {
+        margin: 1em;
+    }
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Posts from '@/components/Posts';
+import Post from '@/components/Post';
 import PostForm from '@/components/PostForm';
 
 Vue.use(Router);
@@ -11,6 +12,11 @@ export default new Router({
             path: '/',
             name: 'Posts',
             component: Posts,
+        },
+        {
+            path: '/post/:id',
+            name: 'Post',
+            component: Post
         },
         {
             path: '/create',
